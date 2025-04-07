6c63cead228007aa7706e537c806b877eb7750ff
Miguel Pafume <leugim.pafume@gmail.com>
2025-04-07 17:51:53 -0300

    analog clock working in real time + more details

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour {
 
+    const float hours_to_degrees = -30.0f, minutes_to_degrees = -6.0f, seconds_to_degrees = -6.0f;
+
     [SerializeField]
     Transform hours_pivot, minutes_pivot, seconds_pivot;
+
+    void Awake () {
+        System.TimeSpan time = System.DateTime.Now.TimeOfDay;
+        hours_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalHours * hours_to_degrees);
+        minutes_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalMinutes * minutes_to_degrees);
+        seconds_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalSeconds * seconds_to_degrees);
+    }
+
+    void Update() {
+        System.TimeSpan time = System.DateTime.Now.TimeOfDay;
+        hours_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalHours * hours_to_degrees);
+        minutes_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalMinutes * minutes_to_degrees);
+        seconds_pivot.localRotation = Quaternion.Euler(0f, 0f, (float)time.TotalSeconds * seconds_to_degrees);
+    }
 }
